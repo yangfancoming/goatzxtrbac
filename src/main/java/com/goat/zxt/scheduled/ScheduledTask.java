f9bf617cf06e12a41f9d2650c82afe5d9a3004d3
yangfan <34465021+jwfl724168@users.noreply.github.com>
2020-12-09 11:00:25 +0800

    代码格式优化

--- src/main/java/com/goat/zxt/scheduled/ScheduledTask.java
+++ src/main/java/com/goat/zxt/scheduled/ScheduledTask.java
@@ -10,14 +10,12 @@ public class ScheduledTask {
 
     private Logger log = LoggerFactory.getLogger(getClass());
 
-
     @Scheduled(fixedDelay = 3000)        //fixedDelay = 5000表示当前方法执行完毕5000ms后，Spring scheduling会再次调用该方法
     public void testinfo() {
         for (int i = 0; i < 1000 * 10; i++) {
             log.info("===info===: 第{}次执行方法", i++);
         }
     }
-
 
     @Scheduled(fixedDelay = 3000)
     public void testdebug() {
